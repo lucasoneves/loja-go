889d07911789bff258565610eb00c35c4ec31c4a
lucasoneves <casluhc@gmail.com>
2024-12-27 16:33:53 -0300

    Struct de produtos

--- main.go
+++ main.go
@@ -8,6 +8,13 @@ import (
 
 var htmlTemplates = template.Must(template.ParseGlob("templates/*.html"))
 
+type Produto struct {
+	Nome       string
+	Descricao  string
+	Preco      float64
+	Quantidade int
+}
+
 func main() {
 	fmt.Println("Bem-vindo a loja Go")
 
@@ -18,5 +25,16 @@ func main() {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	htmlTemplates.Execute(w, "index")
+	produtos := []Produto{
+		{Nome: "Camiseta", Descricao: "Azul, bem bonita", Preco: 39, Quantidade: 5},
+		{"Tenis", "Confortável", 89, 3},
+		{"Fone", "Muito bom", 59, 2},
+		{"Produto novo", "Muito legal", 1.99, 1},
+	}
+
+	err := htmlTemplates.ExecuteTemplate(w, "index.html", produtos)
+	if err != nil {
+		http.Error(w, "Erro ao renderizar o template: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
